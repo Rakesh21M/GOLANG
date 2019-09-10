46c666e7997227940413cd868bfbee39eab98e2d
Rakesh21M <rockmvijay@gmail.com>
2019-09-10 15:51:39 +0530

    Error solved

--- functions/main/main.go
+++ functions/main/main.go
@@ -6,6 +6,11 @@ import (
 	"fmt"
 )
 
+func printSomething() {
+	fmt.Println("THis is something that you asked for!!!")
+}
+
 func main() {
 	fmt.Println("Hello Raki This is Perfect!!")
+	printSomething()
 }
